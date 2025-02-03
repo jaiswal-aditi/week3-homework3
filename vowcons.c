0f302f18e86e123582ec419f2c8c94fd5bdeb9de
Aditi Jaiswal <66053145+jaiswal-aditi@users.noreply.github.com>
2025-02-02 14:45:51 -1000

    Update vowcons.c

--- vowcons.c
+++ vowcons.c
@@ -1,5 +1,7 @@
 /*
 Write a program to check whether a given character is a vowel or a consonant. 
+
+NOTE: Output should **exactly** say either "The entered character is a vowel" or "The entered character is a consonant". 
 */
 
 #include <stdio.h>
